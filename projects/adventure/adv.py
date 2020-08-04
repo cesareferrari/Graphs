0ea2ea03b6a921b34e924fc3d18830e230dd0431
Cesare Ferrari <cesare@maryandferrari.com>
2020-08-04 15:05:02 -0400

    Add get_neighbors

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -1,6 +1,7 @@
 from room import Room
 from player import Player
 from world import World
+from util import Queue
 
 import random
 from ast import literal_eval
@@ -10,10 +11,10 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-map_file = "maps/test_line.txt"
+# map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
-# map_file = "maps/test_loop_fork.txt"
+map_file = "maps/test_loop_fork.txt"
 # map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
@@ -81,7 +82,38 @@ class Graph:
         else:
             self.rooms[room_from.id][direction] = room_to.id
 
+    def get_neighbors(self, room_id):
+        if room_id in self.rooms:
+            return self.rooms[room_id].values()
 
+    # searching for an exit with a '?' as the value. I
+    def bfs(self, starting_room):
+        q = Queue()
+        visited = set()
+        q.enqueue(starting_room.id)
+
+        while q.size() > 0:
+            current_room_id = q.dequeue()
+
+            # if the current room has a free direction return the room
+            directions = self.rooms[current_room_id]
+            if '?' in directions.values():
+                # return room obj with free direction
+                print("visited (set):", visited)
+                return world.rooms[current_room_id]
+
+            if current_room_id not in visited:
+                visited.add(current_room_id)
+
+                neighbors = self.get_neighbors(current_room_id)
+                for neighbor in neighbors:
+                    print("neighbor:", neighbor)
+                    q.enqueue(neighbor)
+
+
+
+# TODO combine with reverse_dirs, make global var.
+reverse = {'n': 's', 'e': 'w', 's': 'n', 'w': 'e'}
 
 
 visited = Graph()
@@ -99,27 +131,40 @@ while len(visited.rooms) < len(world.rooms):
     if direction:
         # log that direction
         traversal_path.append(direction)
-
         # get next room
         next_room = room.get_room_in_direction(direction)
-
         # mark direction in cur_room with next room id
         visited.mark(direction, room, next_room)
-
         # add next room to visited and initialize directions to ?
         visited.add(next_room)
-
         # in next room, mark direction from which we came
         visited.mark(direction, next_room, room, reverse=True)
-
         # travel in the room
         player.current_room = next_room
 
     # When you reach a dead-end (i.e. a room with no unexplored paths), walk back
     # to the nearest room that does contain an unexplored path.
     else:
-        print("player current room:", player.current_room.id)
-        break
+        returned_room = visited.bfs(room)
+        print("walked back to:", returned_room.id)
+        player.current_room = returned_room
+
+        # counter = len(traversal_path) - 1
+
+        # while visited.pick_direction(player.current_room) is None:
+
+        #     back_step = reverse[traversal_path[counter]]
+        #     traversal_path.append(back_step)
+        #     player.travel(back_step)
+        #     counter -= 1
+
+        # player travels in the direction of the reverse of travel_path so far
+        # check if there is a ?
+        # if there is, put the player in that room and loop again from the
+        # beginning
+        # otherwise, move reverse again, until we find a ?
+
+        # break
 
 
 

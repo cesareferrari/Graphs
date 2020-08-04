51b5bf41a3361f67a418ec2d486eed6a80d9c433
Cesare Ferrari <cesare@maryandferrari.com>
2020-08-04 00:02:07 -0400

    Add Graph class for visited

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -10,8 +10,8 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
+# map_file = "maps/test_line.txt"
+map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
 # map_file = "maps/main_maze.txt"
@@ -46,70 +46,73 @@ player.current_room.get_exits()    # returns a list: ['n', 's', 'w', 'e']
 player.travel(direction)           # takes a string 'n', 's', etc.
 """
 
+class Graph:
+    def __init__(self):
+        self.rooms = {}
+
+    def add(self, room):
+        if room.id not in self.rooms:
+            self.rooms[room.id] = {}
+        self.initialize_directions(room)
+
+    def initialize_directions(self, room):
+        for direction in room.get_exits():
+            if direction not in self.rooms[room.id]:
+                self.rooms[room.id][direction] = '?'
+
+    def pick_direction(self, room):
+        possible_directions = []
+        directions = self.rooms[room.id]
+
+        for direction in directions.items():
+            if direction[1] == '?' and direction[1] not in self.rooms:
+                possible_directions.append(direction[0])
+
+        if len(possible_directions) > 0:
+            return random.choice(possible_directions)
+
+        return None
+
 
 reverse = {'n': 's', 'e': 'w', 's': 'n', 'w': 'e'}
 
-# populate directions with ?
-def initialize_directions(room):
-    for direction in room.get_exits():
-        if direction not in visited[room.id]:
-            visited[room.id][direction] = '?'
 
+visited = Graph()
 
-def add(room, visited):
-    if room.id not in visited:
-        visited[room.id] = {}
-
-    initialize_directions(room)
-
-
-def pick_direction(visited, room):
-    possible_directions = []
-    directions = visited[room.id]
-
-    for direction in directions.items():
-        if direction[1] == '?' and direction[1] not in visited:
-            possible_directions.append(direction[0])
-
-    if len(possible_directions) > 0:
-        return random.choice(possible_directions)
-
-    return None
-
-
-visited = {}
-
-while len(visited) < len(world.rooms):
+while len(visited.rooms) < len(world.rooms):
     # Start in room 0
     room = player.current_room
     # add room to our graph
-    add(room, visited)
+    # add(room, visited)
+    visited.add(room)
 
     # pick a random unexplored direction from the player current room
-    direction = pick_direction(visited, room)
+    direction = visited.pick_direction(room)
 
     if direction:
         # log that direction
         traversal_path.append(direction)
-
         # get next room
         next_room = room.get_room_in_direction(direction)
-
         # mark direction in cur_room with next room id
-        visited[room.id][direction] = next_room.id
-        
+        visited.rooms[room.id][direction] = next_room.id
         # add next room to visited and initialize directions to ?
-        add(next_room, visited)
+
+        # add(next_room, visited)
+        visited.add(next_room)
 
         # in next room, mark direction from which we came
-        visited[next_room.id][reverse[direction]] = room.id
-        # print(f"Current room: {room.id}\nCur room directions: {visited[room.id]}\nNext room: {next_room.id}\nNext room directions: {visited[next_room.id]}")
+        visited.rooms[next_room.id][reverse[direction]] = room.id
+        # travel in the room
+        player.current_room = next_room
 
-    # travel in the room
-    player.current_room = next_room
+    # When you reach a dead-end (i.e. a room with no unexplored paths), walk back
+    # to the nearest room that does contain an unexplored path.
+    else:
+        print("player current room:", player.current_room.id)
+        break
 
 
-        # loop
 
 
 

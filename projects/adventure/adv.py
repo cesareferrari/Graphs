48711a6e254d0e99eb96b5ee214e3a9b7e5f5ec5
Cesare Ferrari <cesare@maryandferrari.com>
2020-08-04 10:19:39 -0400

    Add mark method to Graph class

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -10,8 +10,8 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-# map_file = "maps/test_line.txt"
-map_file = "maps/test_cross.txt"
+map_file = "maps/test_line.txt"
+# map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
 # map_file = "maps/main_maze.txt"
@@ -73,8 +73,15 @@ class Graph:
 
         return None
 
+    def mark(self, direction, room_from, room_to, reverse=False):
+        reverse_dirs = {'n': 's', 'e': 'w', 's': 'n', 'w': 'e'}
 
-reverse = {'n': 's', 'e': 'w', 's': 'n', 'w': 'e'}
+        if reverse:
+            self.rooms[room_from.id][reverse_dirs[direction]] = room_to.id
+        else:
+            self.rooms[room_from.id][direction] = room_to.id
+
+
 
 
 visited = Graph()
@@ -92,17 +99,19 @@ while len(visited.rooms) < len(world.rooms):
     if direction:
         # log that direction
         traversal_path.append(direction)
+
         # get next room
         next_room = room.get_room_in_direction(direction)
-        # mark direction in cur_room with next room id
-        visited.rooms[room.id][direction] = next_room.id
-        # add next room to visited and initialize directions to ?
 
-        # add(next_room, visited)
+        # mark direction in cur_room with next room id
+        visited.mark(direction, room, next_room)
+
+        # add next room to visited and initialize directions to ?
         visited.add(next_room)
 
         # in next room, mark direction from which we came
-        visited.rooms[next_room.id][reverse[direction]] = room.id
+        visited.mark(direction, next_room, room, reverse=True)
+
         # travel in the room
         player.current_room = next_room
 
